66c69a57f641d3ca68792dd86c8674565d97e2e6
Joel Hales <joelh@prioritydispatch.net>
2024-07-23 15:52:12 -0600

    update to v0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <UpdateHandler />
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center min-h-screen bg-blue-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
       </div>
     </>
